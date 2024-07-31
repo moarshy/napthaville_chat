2fb79242fc359d203a1140ab1e02125d4b4db06e
moarshy <marshath@gmail.com>
2024-07-31 14:21:32 +0800

    add get_utterence

--- napthaville_chat/run.py
+++ napthaville_chat/run.py
@@ -46,6 +46,8 @@ async def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None
             'persona_name': init_persona_name,
         }
     )
+    init_persona_info = json.loads(init_persona_info)
+    # target_persona_info: {"name": "Maria Lopez", "act_description": "sleeping"}
 
     target_persona_info = await target_persona_info_task(
         task='get_personal_info', 
@@ -53,32 +55,41 @@ async def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None
             'persona_name': target_persona_name,
         }
     )
+    target_persona_info = json.loads(target_persona_info)
 
     logger.info(f"init_persona_info: {init_persona_info}")
     logger.info(f"target_persona_info: {target_persona_info}")
     logger.info(f"Type of init_persona_info: {type(init_persona_info)}")
 
-    # init_persona_chat_params = {
-    #     'init_persona': init_persona_name,
-    #     'target_persona': target_persona_name,
-    #     'maze_folder': inputs.maze_folder,
-    # }
-
-    # target_persona_chat_params = {
-    #     'init_persona': target_persona_name,
-    #     'target_persona': init_persona_name,
-    #     'maze_folder': inputs.maze_folder,
-    # }
-
-    # init_persona_chat_task = ...
-    # target_persona_chat_task = ...
-
-    # curr_chat = []
-    # for i in range(8):
-
-    results = {
-        'init_persona_info': init_persona_info,
-        'target_persona_info': target_persona_info,
+    init_persona_chat_params = {
+        'init_persona_name': init_persona_name,
+        'target_persona_name': target_persona_info['name'],
+        'target_persona_description': target_persona_info['act_description'],
     }
 
-    return json.dumps(results)
+    target_persona_chat_params = {
+        'init_persona_name': target_persona_name,
+        'target_persona_name': init_persona_info['name'],
+        'target_persona_description': init_persona_info['act_description'],
+    }
+
+    curr_chat = []
+    for i in range(8):
+        init_persona_chat_params['curr_chat'] = json.dumps(curr_chat)
+        init_utterance = await init_persona_info_task(
+            task='get_utterence',
+            task_params=init_persona_chat_params,
+        )
+        logger.info(f"init_utterance: {init_utterance}")
+
+        target_persona_chat_params['curr_chat'] = json.dumps(json.loads(init_utterance)['curr_chat'])
+        target_utterance = await target_persona_info_task(
+            task='get_utterence',
+            task_params=target_persona_chat_params,
+        )
+        logger.info(f"target_utterance: {target_utterance}")
+
+        curr_chat = json.loads(target_utterance)['curr_chat']
+        logger.info(f"curr_chat: {curr_chat}")
+
+    return json.dumps(curr_chat)
